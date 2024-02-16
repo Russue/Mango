b814a8ad265e530c01cbfaa767510413e64c9ce8
Milan <markchristians92@gmail.com>
2024-02-16 17:28:14 -0600

    Added

--- Calculator.java
+++ Calculator.java
@@ -7,8 +7,4 @@ public class Calculator {
     public static int subtraction(int num1, int num2) {
         return num1 - num2;
     }
-     // Multiplication method
-    public static int multiplication(int num1, int num2) {
-        return num1 * num2;
-    }
 }
